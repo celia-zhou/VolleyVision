c062aa22639382c2b8c540dda4a4f6dff74bb5f2
Celia Zhou <celiaxyzhou@gmail.com>
2021-10-07 22:23:07 -0500

    created initial searchbar component

--- volleyvision-app/src/pages/player_dashboard.js
+++ volleyvision-app/src/pages/player_dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import SearchBar from '../components/Topbar/Searchbar'
 import SideBar from '../components/Sidebar/Sidebar'
 
 const Dashboard = () => {
     return (
         <div>
             <SideBar/>
+            <SearchBar/>
         </div>
     )
 }
