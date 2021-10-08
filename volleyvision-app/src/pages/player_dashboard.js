203d07f612db7924008ab5b5dad1cbbe875cae51
Celia Zhou <celiaxyzhou@gmail.com>
2021-10-07 22:00:39 -0500

    created sidebar component

---
+++ volleyvision-app/src/pages/player_dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SideBar from '../components/Sidebar/Sidebar'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <SideBar/>
+        </div>
+    )
+}
+
+export default Dashboard
