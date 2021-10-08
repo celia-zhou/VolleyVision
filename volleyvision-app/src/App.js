2e48c2a8c7342f68befc75b75d4493becc0ca84e
Celia Zhou <celiaxyzhou@gmail.com>
2021-10-07 21:56:01 -0500

    logo place test

--- volleyvision-app/src/App.js
+++ volleyvision-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './images/logo.svg';
 import './styles/App.css';
 import Dashboard from "./pages/player_dashboard";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -6,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      
+     
       <Dashboard />
   
     </Router>
