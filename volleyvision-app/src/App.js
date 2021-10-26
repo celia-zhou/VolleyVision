35df421524ebf34bc2836ca9a5033a5962c77bca
Michelle Liu <michelle.liu@vanderbilt.edu>
2021-10-26 18:43:41 -0500

    profile pages and recuiter pages

--- volleyvision-app/src/App.js
+++ volleyvision-app/src/App.js
@@ -1,12 +1,20 @@
 import './styles/App.css';
 import Dashboard from "./pages/player_dashboard";
+import Profile from "./pages/profile_page";
+import RecruiterTeam from './pages/recruiter_team';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
      
-      <Dashboard />
+      <Switch>
+          <Route path="/Profile" component={Profile} />
+      </Switch>
+
+      <Switch>
+          <Route path="/RecruiterTeam" component={RecruiterTeam} />
+      </Switch>
   
     </Router>
   );
---
+++ volleyvision-app/src/pages/recruiter_team.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import SearchBar from '../components/Topbar/Searchbar'
+import SideBar from '../components/Sidebar/Sidebar'
+import styled from 'styled-components'
+import { ListItemText } from '@material-ui/core'
+import photo from './vandy.jpeg'
+import videoScreenshot1 from './videoscreenshot1.png'
+import videoScreenshot2 from './videoscreenshot2.png'
+
+console.log(photo)
+
+const Container = styled.div`
+    position: absolute;
+    left: 650px;
+    top: 65px;
+    font: Times New Roman;
+    font-size: 30px;
+    font-weight: bold;
+    width: 400px;
+    height: 150px;
+`
+
+
+const SubtitleContainer = styled.div`
+    position: absolute;
+    left: 400px;
+    top: 530px;
+    font: Times New Roman;
+    font-size: 30px;
+    font-style: italic;
+    width: 300px;
+    height: 150px;
+`
+
+const DescriptionContainer = styled.div`
+    position: absolute;
+    left: 400px;
+    top: 580px;
+    font: Times New Roman;
+    font-size: 30px;
+    width: 300px;
+    height: 150px;
+`
+
+const InfoContainer = styled.div`
+    position: absolute;
+    left: 850px;
+    top: 525px;
+    font: Times New Roman;
+    font-size: 30px;
+    width: 300px;
+    height: 150px;
+    font-weight: bold;
+`
+
+const ImageContainer = styled.div`
+    position: absolute;
+    left: 400px;
+    top: 150px;
+    width: 400px;
+    height: 150px;
+`
+
+const VideoTitleContainer = styled.div`
+    position: absolute;
+    left: 825px;
+    top: 630px;
+    font: Times New Roman;
+    font-size: 30px;
+    font-style: italic;
+    width: 300px;
+    height: 150px;
+`
+
+const VideoThumbnailContainer = styled.div`
+    position: absolute;
+    left: 800px;
+    top: 625px;
+    width: auto;
+    height: auto;
+`
+
+const RecruiterTeam = () => {
+    return (
+        <div>
+            <SideBar/>
+            <Container>
+                Vanderbilt University
+            </Container>
+
+            <ImageContainer>
+                <img src={photo} />;
+            </ImageContainer>
+
+            <SubtitleContainer>
+                Our Team Philosophy
+            </SubtitleContainer>
+
+            <DescriptionContainer>
+                <ListItemText>Our team at Vanderbilt University is dedicated to the collective improvement of our members.
+                    We place a focus on teamwork and collaboration - no player gets left behind. We preach hard work, tenacity, and grit.
+                    At Vanderbilt, our team members receive a world-class education while playing on an award-winning Volleyball team.</ListItemText>
+                
+            </DescriptionContainer>
+            
+            <InfoContainer>
+                <ListItemText>Location: Nashville, TN</ListItemText>
+                <ListItemText>Team Coach: Celia Zhou</ListItemText>
+            </InfoContainer>
+
+            <VideoTitleContainer>Team Highlights</VideoTitleContainer>
+
+            <VideoThumbnailContainer>
+                <img src={videoScreenshot1} />;
+            </VideoThumbnailContainer>
+
+            <SearchBar/>
+    
+        </div>
+
+        
+    )
+}
+
+export default RecruiterTeam
