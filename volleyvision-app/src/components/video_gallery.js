0ef3c08eff3dcff57e9d8d7b6b290abad691e7d2
kitano26 <kkstuff18@gmail.com>
2021-10-27 23:20:23 -0500

    Video Gallery

--- volleyvision-app/src/components/video_gallery.js
+++ volleyvision-app/src/components/video_gallery.js
@@ -1,32 +1,3 @@
-// import React from "react";
-// import ImageList from '@mui/material/ImageList';
-// import ImageListItem from '@mui/material/ImageListItem';
-// import ImageListItemBar from '@mui/material/ImageListItemBar';
-// import ReactPlayer from "react-player";
-
-// function VideoGallery() {
-//     return (
-//       <div>
-//           {/* <ImageList sx={{ width: 1160, height: 1100 }} cols={3}> */}
-//           <ImageList sx={{ width: 1160, height: 100 }} cols={3}>
-//               {itemData.map((item) => (
-//                   <ImageListItem sx={{pb: 1}} key={item.urlLink}>
-//                   <ReactPlayer
-//                       url={item.urlLink}
-//                       controls
-//                       width = "350px"
-//                       height = "300px"
-//                   />
-//                   <ImageListItemBar sx={{pb: 2}} position="below" title={item.name} />
-//                   </ImageListItem>
-//               ))}
-//           </ImageList>
-//       </div>
-//     )
-//   }
-// export default VideoGallery
-
-
 import React from 'react';
 import ReactPlayer from "react-player";
 import ImageList from '@mui/material/ImageList';
