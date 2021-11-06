5ffd0ecd138c723cd504cd68cca3c3507f47d33a
Pei-Shan-Tan <44717517+Pei-Shan-Tan@users.noreply.github.com>
2021-11-05 19:48:35 -0500

    authreducer cases

--- volleyvision-app/src/store/reducers/authReducer.js
+++ volleyvision-app/src/store/reducers/authReducer.js
@@ -1,7 +1,46 @@
 const initState = {}
 
 const authReducer = (state = initState,action) => {
-    return state
+    switch(action.type){
+        case 'SU_SUCCESS':
+            console.log('signup success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SU_FAIL':
+            console.log('signup fail');
+            return {
+                ...state,
+                authError: 'signupfailed'
+            }
+            case 'LI_SUCCESS':
+                console.log('login success');
+                return {
+                    ...state,
+                    authError: null
+                }
+            case 'LI_FAIL':
+                console.log('login fail');
+                return {
+                    ...state,
+                    authError: 'loginfailed'
+                }
+                case 'LO_SUCCESS':
+                    console.log('logout success');
+                    return {
+                        ...state,
+                        authError: null
+                    }
+                case 'LO_FAIL':
+                    console.log('logout fail');
+                    return {
+                        ...state,
+                        authError: 'logoutfailed'
+                    }
+        default:
+            return state;
+    }
 }
 
 export default authReducer
--- volleyvision-app/src/store/reducers/rootReducer.js
+++ volleyvision-app/src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from "./authReducer";
 import projReducer from "./projReducer";
 import { combineReducers } from "redux";
+import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    project: projReducer
+    project: projReducer,
+    firestore: firestoreReducer
 })
 
 export default rootReducer
