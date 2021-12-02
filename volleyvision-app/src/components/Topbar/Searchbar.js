bb395f8cccf6a518f06e589b59b6a0123581c59e
Celia Zhou <celiazhou@Celias-MacBook-Pro-M1.local>
2021-12-02 16:19:34 -0600

    removed notifications from searchbar

--- volleyvision-app/src/components/Topbar/Searchbar.js
+++ volleyvision-app/src/components/Topbar/Searchbar.js
@@ -190,15 +190,11 @@ export default function PrimarySearchAppBar() {
           </div>
 
           <div className={classes.sectionDesktop}>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
+            <IconButton aria-label="mail" color="inherit">
                 <MailIcon />
-              </Badge>
             </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
+            <IconButton aria-label="notifications" color="inherit">
                 <NotificationsIcon />
-              </Badge>
             </IconButton>
             <IconButton
               edge="end"
