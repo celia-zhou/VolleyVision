54b504d44cb16eb0ed4e7ae6d4694d7d1b92d5f6
kitano26 <kkstuff18@gmail.com>
2021-12-05 22:19:36 -0600

    Fixed YOUTUBE

--- volleyvision-app/src/components/Videos/ytVideos.js
+++ volleyvision-app/src/components/Videos/ytVideos.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactPlayer from "react-player";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -10,7 +10,6 @@ function getAccessToken() {
 
   if (currToken != null && currToken != sessionStorage.getItem('accessToken')) {
     sessionStorage.setItem('accessToken', currToken);
-    
   }
 
   var xhr = new XMLHttpRequest();
@@ -70,18 +69,72 @@ function createVideoElems() {
 }
 
 export default function YTVideos() {
+  let [videoElems, setVideoElems] = useState([]);
 
   useEffect(() => {
-    getAccessToken();
-  }, [])
+    const getVideoState = async () => {
+      if (sessionStorage.getItem('accessToken') === null) {
+        var currToken = new URLSearchParams(window.location.hash).get('access_token');
   
+        if (currToken !== null && currToken !== sessionStorage.getItem('accessToken')) {
+          sessionStorage.setItem('accessToken', currToken);
+        }
+      
+        const response = await fetch('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&access_token=' + sessionStorage.getItem('accessToken'));
+        if (response.status === 200) {
+          let jsonResp = await response.json();
+          sessionStorage.setItem('playlistID', jsonResp.items[0].contentDetails.relatedPlaylists.uploads);
+        } else if (response.status === 403) {
+          alert("Access denied. Must authorize access to a Youtube account.");
+        }
+        /*
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET',
+              'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&access_token=' + sessionStorage.getItem('accessToken'));
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+              var jsonResp = JSON.parse(xhr.response);
+              sessionStorage.setItem('playlistID', jsonResp.items[0].contentDetails.relatedPlaylists.uploads);
+            }
+            else if (xhr.readyState === 4 && xhr.status === 403) {
+              // Token invalid, so prompt for user permission.
+              alert("Access denied. Must authorize access to a Youtube account.");
+            }
+        };
+      
+        xhr.send(null);
+        */
+      }
 
-  if (sessionStorage.getItem('accessToken') != null) {
-    getUploads();
-  
-    if (sessionStorage.getItem('uploadedVideos') != null) {
-      const videoElems = createVideoElems();
-      return (
+      if (sessionStorage.getItem('accessToken') !== null) {
+        const response = await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&access_token=' + sessionStorage.getItem('accessToken')+'&playlistId='+sessionStorage.getItem('playlistID'));
+        if (response.ok) {
+          let data = await response.json();
+          let jsonVideos = data.items;
+
+          let videoIdArr = [];
+          for(let i = 0; i < jsonVideos.length; i++) {
+            videoIdArr.push({id: jsonVideos[i].contentDetails.videoId, title: jsonVideos[i].snippet.title});
+          }
+          const videoElems = [];
+        
+          for(let i = 0; i < videoIdArr.length; i++) {
+            videoElems.push(    {
+              urlLink: `https://www.youtube.com/watch?v=${videoIdArr[i].id}`,
+              title: videoIdArr[i].title
+          })
+        }
+        setVideoElems(videoElems)
+      }
+      }
+    };
+    getVideoState();
+  }, [])
+
+  if (videoElems.length === 0) {
+    return null;
+  } else {
+        return (
         <div>
             <ImageList sx={{ position: 'absolute', top: 220, left: 300, width: 1100, height: 1100, bgcolor:"#DBEBFB"}} cols={3}>
             {videoElems.map((item) => (
@@ -96,9 +149,7 @@ export default function YTVideos() {
             ))}
             </ImageList>
         </div>
-      );
-    }
-  }
-  
-  return null;
+      ); 
+
+            }
 }
