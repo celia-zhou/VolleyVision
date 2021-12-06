c5911a9b992321f88a91178d905bcf5d19753fc5
Pei-Shan-Tan <44717517+Pei-Shan-Tan@users.noreply.github.com>
2021-12-06 17:42:59 -0600

    navbar now a component

--- volleyvision-app/src/components/NavBar/navbar.js
+++ volleyvision-app/src/components/NavBar/navbar.js
@@ -1,3 +1,25 @@
-// need to change hardcoded nav bar to component
+import React from "react"
+import logo from '../../images/logo-clear.png'
 
-// also need to change navbar from css to react
+export default function NavBar({}
+    ){
+        return(
+            <div className="header-container">
+            <div className="nav-bar">
+                <ul>
+                    <div className="links">
+                        <li><a href="/login">Login</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="https://rpmsand.com/about/">About</a></li>
+                        <li><a href="/home" >Home</a></li>
+                    </div>
+                    <li className="left-li" style={{float:'left'}} >
+                        <a href="/" style={{paddingTop:'0px', paddingLeft:'5%', paddingBottom: '0px'}}>
+                            <img className="logo" alt="logo" src={logo} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        )
+    }
