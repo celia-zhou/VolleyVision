b8c905afec19f9904537b7ff33c06121dd471ac1
Pei-Shan-Tan <44717517+Pei-Shan-Tan@users.noreply.github.com>
2021-12-06 17:43:07 -0600

    using the navbar

--- volleyvision-app/src/pages/home.js
+++ volleyvision-app/src/pages/home.js
@@ -1,34 +1,18 @@
 // NPM
 import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
 
 // styles
 import '../styles/home.css'
 
 //images
-import logo from '../images/logo-clear.png';
 //import phgrey from '../images/placeholder-gray.png';
 
 
 const Home = () => {
     return (
         <div>
-            <div className="header-container">
-                    <div className="nav-bar">
-                        <ul>
-                            <div className="links">
-                                <li><a href="/login">Login</a></li>
-                                <li><a href="/contact">Contact</a></li>
-                                <li><a href="https://rpmsand.com/about/">About</a></li>
-                                <li><a href="/home" >Home</a></li>
-                            </div>
-                            <li className="left-li" style={{float:'left'}} >
-                                <a href="/" style={{paddingTop:'0px', paddingLeft:'5%', paddingBottom: '0px'}}>
-                                    <img className="logo" alt="logo" src={logo} />
-                                </a>
-                            </li>
-                        </ul>
-                </div>
-            </div>
+            <NavBar />
             <div className="body-container" id="home">
                 <div className="announcement" style={{textAlign: 'center'}}>
                     <br /><br /><br /><br />
--- volleyvision-app/src/pages/login.js
+++ volleyvision-app/src/pages/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginForm from "../components/AccountAuth/forms/loginform";
+import NavBar from "../components/NavBar/NavBar";
 
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "../components/AccountAuth/authcontext";
@@ -15,23 +16,7 @@ import logo from '../images/logo-clear.png';
 export default function Login() {
     return (
         <div>
-            <div className="header-container">
-                <div className="nav-bar">
-                    <ul>
-                        <div className="links">
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="https://rpmsand.com/contact/">Contact     </a></li>
-                            <li><a href="https://rpmsand.com/about/">About    </a></li>
-                            <li><a href="/home">Home    </a></li>
-                        </div>
-                        <li className="left-li" style={{float:'left'}} >
-                            <a href="/" style={{paddingTop:'0px', paddingLeft:'5%', paddingBottom: '0px'}}>
-                                <img className="logo" alt="logo" src={logo} />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <NavBar />
             <div className="body-container" id="homebody">
                 <AuthProvider>
                     <Container
